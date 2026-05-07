a87cf792effaf4a3b3497b9dd82f5dc7ed1b6f83
Aditya Kumar Sharma <adityasam2007@gmail.com>
2026-05-07 11:11:11 +0530

    Fixed Vercel status type error

--- frontend/app/job/[id]/page.tsx
+++ frontend/app/job/[id]/page.tsx
@@ -357,7 +357,7 @@ export default function JobPage() {
       {/* Status */}
       {job && (
         <StatusCard
-          status={normalizedStatus}
+          status={normalizedStatus as any}
           jobId={id}
           pollCount={pollCount}
         />
